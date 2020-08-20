c5cbccba49e18f74918a0e17968379aaa3f07d4f
Avelcorvin <52276079+Avelcorvin@users.noreply.github.com>
2020-08-20 16:09:13 +0300

    task8 completed

--- src/es-features/task8.js
+++ src/es-features/task8.js
@@ -28,20 +28,16 @@ export function task8Old() {
 // Напишите реализацию функции task8Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task8New() {
-  const obj = {
+export const task8New = () => {
+  var obj = {
     nums: [0, 2, 5, 10, 15],
     fives: [],
     calculateNumsFive() {
-      this.nums.forEach(function(v) {
-        if (v % 5 === 0) {
-          this.fives.push(v);
-        }
+      this.nums.forEach(v => {
+        if (v % 5 === 0) this.fives.push(v);
       });
-
       return this.fives;
     },
   };
-
   return obj.calculateNumsFive();
-}
+};
