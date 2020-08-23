5bf964eb1b6ca125ddf9c2071ed550aacf9eadcd
Avelcorvin <52276079+Avelcorvin@users.noreply.github.com>
2020-08-23 19:11:58 +0300

    array-practice/task2 completed

--- src/index.js
+++ src/index.js
@@ -32,6 +32,8 @@ import { task10Old, task10New } from './es-features/task10';
 // import { task14Old, task14New } from './es-features/task14';
 // import { task15Old, task15New } from './es-features/task15';
 import { any } from './array-practice/task1';
+import { arrayDiff } from './array-practice/task2';
+import { forEachRight } from './array-practice/task3';
 
 console.log('task1 old', task1Old(10) === 500); // true
 console.log('task1 new', task1New(10) === 500); // true
@@ -108,7 +110,14 @@ console.log('=============');
 
 // console.log('=============');
 
-console.log('task1 array-pracice', any([0, 1, 2, 0], x => x >= 2)); //-> true
-console.log('task1 array-pracice', any([0, 0, 1, 0])); //-> true
-console.log('task1 array-pracice', any([0, 0, 0, 0])); //-> false
+console.log('any', any([0, 1, 2, 0], x => x >= 2)); //-> true
+console.log('any', any([0, 0, 1, 0])); //-> true
+console.log('any', any([0, 0, 0, 0])); //-> false
+console.log('=============');
+
+console.log('arrayDiff', arrayDiff([1, 2, 3], [1, 2, 4])); //-> [3, 4]
+console.log('arrayDiff', arrayDiff([1, 3, 3, 4], [1, 3, '4'])); // -> [4, '4']
+console.log('=============');
+
+forEachRight([1, 2, 3, 4], val => console.log(val)); // -> в консоль 4 3 2 1
 console.log('=============');
