a27df2e42adbf9361e48aad8061966295f8ccd6c
Avelcorvin <52276079+Avelcorvin@users.noreply.github.com>
2020-08-23 20:55:43 +0300

    array-practice/task9 completed

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ import { createGenerator } from './array-practice/task5';
 import { transformArrayToNumber } from './array-practice/task6';
 import { take } from './array-practice/task7';
 import { without } from './array-practice/task8';
-import {} from './array-practice/task9';
+import { indexOfAll } from './array-practice/task9';
 import {} from './array-practice/task10';
 
 console.log('task1 old', task1Old(10) === 500); // true
@@ -170,4 +170,8 @@ console.log('=============');
 
 console.log(without([2, 1, 2, 3], 1, 2)); //-> [3]
 console.log(without([2, 1, 10, 20], 1, 2)); //-> [10, 20]
+console.log('=============');
+
+console.log(indexOfAll([1, 2, 3, 1, 2, 3], 1)); // -> [0, 3]
+console.log(indexOfAll([1, 2, 3], 4)); // -> []
 console.log('=============');
